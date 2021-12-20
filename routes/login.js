5d887e9225ccbf69a96fdb766eb3be4d1469e520
sixto-c <csixto228@gmail.com>
2021-12-20 11:37:51 -0300

    cambios en el login

--- routes/login.js
+++ routes/login.js
@@ -1,45 +1,47 @@
 var express = require('express');
 var router = express.Router();
-const {auth} = require('./../models/cursos'); 
+const {auth} = require('../models/cursos'); 
 const sha1 = require('sha1');
 
-const get = (req, res)=>{res.render('login');}
-
-const login = async(req, res)=>{
-    try{
-        req.body.password = sha1(req.body.password);
-        var obj = req.body;
-        var result = await auth(obj);
-        console.log(result);
-        if(result.length == 0){
-            res.render('login', {message: "user o pass incorrecta"});
-     
-        }
-        const [{id, admin}] = result;
-        if(admin === 1){
-            const [{id, admin}] = result;
-            console.log(id, admin);
-            req.session.idUser = id;
-            req.session.admin = admin;
-            res.redirect('/perfilAdmin');
-
-        }
-        else if(admin === 0){
-            const [{id, admin}] = result;
-            console.log(id, admin);
-            req.session.idUser = id;
-            req.session.admin = admin;
-            res.redirect('/cursos')
-
-        }            
-        
-    }
-    catch(e){
-        console.log(e);
-    }
+const getLogin = async(req, res)=>{
+    res.render('login');
 }
 
-router.get('/', get);
-router.post('/', login);//como mi action va a ser en login. y yo estoy en login.js no va ningun palabra en ('/')
+const login = async(req, res)=>{
+    req.body.pass = sha1(req.body["pass"]);
+    var obj = req.body;
+    console.log(obj);
+    var result = await auth(obj);
+    console.log(result);
+    if(result.length === 0){
+        res.render('login', {message : 'user o pass incorrectas'});
+    }
+    
+    const [{id, admin}] = result;
+    console.log(id, admin);
+    req.session.idUser = id;
+    req.session.admin = admin;
+
+    if(result.length != 0 && req.session.admin  === 1){
+        
+      
+        res.redirect('/cursos');
+
+    }
+    else if(result.length != 0 && req.session.admin === 0){
+
+        res.redirect('/'); 
+    }
+
+   
+}
+
+
+
+
+
+
+router.get('/', getLogin);
+router.post('/', login);
+
 module.exports = router;
- 
